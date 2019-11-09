0a7ffcd1a536ca2816edfab61ebb7441e5db28c8
Brent Lamb <blamb001@fathomgame.com>
2019-11-09 00:43:47 -0800

    Start of prefabs for Entities

---
+++ Ported/blacklamb/Factory/Assets/Scripts/Components/Bot.cs
@@ -0,0 +1,37 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Factory.Components
+{
+    public struct BotComponent : IComponentData
+    {
+        public float radius;
+        public int hitCount;
+        public Entity targetCrafter;
+        public bool movingToResource;
+        public bool holdingResource;
+        public Color botColor;
+        public float botSpeed;
+    }
+
+    namespace Author
+    {
+        public class Bot : MonoBehaviour, IConvertGameObjectToEntity
+        {
+            public float radius;
+            public float botSpeed = 2.0f;
+
+            public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+            {
+                BotComponent bot = new BotComponent()
+                {
+                    radius = radius,
+                    hitCount = 0,
+                    botSpeed = botSpeed,
+                    botColor = Color.white
+                };
+                dstManager.AddComponentData(entity, bot);
+            }
+        }
+    }
+}
---
+++ Ported/blacklamb/Factory/Assets/Scripts/Components/Tile.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Entities;
+using Factory.Types;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace Factory.Components
+{
+    public struct TileComponent : IComponentData
+    {
+        public int moveCost;
+        public bool isResourceSpawner;
+        public Types.PathType pathType;
+    }
+
+    namespace Author
+    {
+        public class Tile : MonoBehaviour, IConvertGameObjectToEntity
+        {
+            public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
