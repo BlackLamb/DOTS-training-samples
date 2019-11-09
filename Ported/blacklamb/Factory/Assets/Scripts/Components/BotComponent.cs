ba30c102369c3fd83af038a9f5eeb52d7bd49d83
Brent Lamb <blamb@crypticstudios.com>
2019-11-08 17:05:16 -0800

    More component work

---
+++ Ported/blacklamb/Factory/Assets/Scripts/Components/BotComponent.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Factory.Components
+{
+    public struct BotComponent : IComponentData
+    {
+        public float radius;
+        public int hitCount;
+        public Entity targetCrafter;
+        public bool movingToResource;
+        public bool holdingResource;
+    }
+
+    public class Bot : MonoBehaviour, IConvertGameObjectToEntity
+    {
+        public float radius;
+        
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            BotComponent bot = new BotComponent() {radius = radius, hitCount = 0};
+            dstManager.AddComponentData(entity, bot);
+        }
+    }
+}
